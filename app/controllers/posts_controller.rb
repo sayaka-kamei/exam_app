ca2c34b390ec2d66c05aade22cfc92f5b3bc4da1
sayaka kamei <sayukou316@gmail.com>
2022-10-01 10:58:31 +0900

    Strong Parametersを定義した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,13 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
-    Post.create(content: params[:post][:content]) 
+    Post.create(post_params) 
     redirect_to new_post_path
-  end    
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end  
 end
