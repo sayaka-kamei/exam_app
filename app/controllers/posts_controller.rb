559012b3cb1a94dfa0a70782611159948d46a39d
sayaka kamei <sayukou316@gmail.com>
2022-10-01 15:23:27 +0900

    パーシャルを作成しViewを共通化した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
@@ -15,13 +17,10 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end  
   def edit
-    @post = Post.find(params[:id])
   end
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "投稿を編集しました！"
     else
@@ -32,5 +31,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
-  end  
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
